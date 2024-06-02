1c34f117aa3b59c8883cebabfa5f14fc343d52e6
nassdragon <95158877+nassdragon@users.noreply.github.com>
2024-06-02 21:51:54 +0700

    Add files via upload

---
+++ car.js
@@ -0,0 +1,10 @@
+function play() {
+    const car = document.querySelector('.car')
+    car.style.animationPlayState = 'running'
+}
+
+function pause() {
+    const car = document.querySelector('.car')
+    car.style.animationPlayState = 'paused'
+
+}
